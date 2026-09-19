0a5f4fc903577496b803d78ec86775478aa6ea32
nfruiz23-lang <nfruiz23@itp.edu.co>
2026-09-19 15:14:43 -0500

    feat: agregar resta a Calculator

--- src/test.py
+++ src/test.py
@@ -1,5 +1,9 @@
-from main import calculator
+from main import Calculator
 
-def test_sum():
-    calc = calculator()
-    assert calc.sum(2, 2) == 4
+
+def test_suma_2_numbers():
+    assert Calculator().suma(2, 2) == 4
+
+
+def test_resta_2_numbers():
+    assert Calculator().resta(5, 3) == 2
